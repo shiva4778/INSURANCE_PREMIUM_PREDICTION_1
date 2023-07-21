ce411a491e0e2f1a642134c526522600c35934ee
Shivanshu Mall <shivanshumall786@gmail.com>
2023-07-21 17:33:27 +0530

    dsgv

--- application.py
+++ application.py
@@ -1,10 +1,9 @@
-### Integrate HTML With Flask
-### HTTP verb GET And POST
+# app.py
+from flask import Flask, redirect, url_for, render_template, request
 from insurance.logger import logging
-from flask import Flask,redirect,url_for,render_template,request
-from insurance.pipeline.predict_pipeline import CustomData,PredictPipeline
+from insurance.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-application=Flask(__name__)
+application = Flask(__name__)
 
 @application.route('/')
 def welcome():
@@ -12,32 +11,23 @@ def welcome():
 
 @application.route('/success/<int:score>')
 def success(score):
-    
+    return render_template('result.html', result=score)
 
-    return render_template('result.html',result=score)
-
-### Result checker submit html page
-@application.route('/submit',methods=['POST','GET'])
+@application.route('/submit', methods=['POST', 'GET'])
 def submit():
-    predicted_value=0
-    if request.method=='POST':
-        age=float(request.form['age'])
-        sex=(request.form['sex'])
-        bmi=float(request.form['bmi'])
-        children=float((request.form['children']))
-        smoker=(request.form['smoker'])
-        region=(request.form['region'])
-        print('data captured:',age,sex,bmi,children,smoker,region)
-        features=CustomData(age=age,sex=sex,bmi=bmi,children=children,smoker=smoker,region=region).get_data_as_data_frame()
-        predicted_value=PredictPipeline().predict(features)
+    predicted_value = 0
+    if request.method == 'POST':
+        age = float(request.form['age'])
+        sex = request.form['sex']
+        bmi = float(request.form['bmi'])
+        children = float(request.form['children'])
+        smoker = request.form['smoker']
+        region = request.form['region']
+        print('data captured:', age, sex, bmi, children, smoker, region)
+        features = CustomData(age=age, sex=sex, bmi=bmi, children=children, smoker=smoker, region=region).get_data_as_data_frame()
+        predicted_value = PredictPipeline().predict(features)
 
-    return render_template('result.html',result=predicted_value[0])
-    
-    #return redirect(url_for('success',score=predicted_value))
+    return render_template('result.html', result=predicted_value[0])
 
-    
-
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     application.run(host='0.0.0.0')
